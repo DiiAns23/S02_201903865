02fd819b2011f6e943b527cdee87f774824c26be
Diego Andrés Obín Rosales <2742934200101@ingenieria.usac.edu.gt>
2023-02-27 21:37:36 -0600

    feat: Se realizó la completación del proyecto
    Se agregó el contenido del backend al fronten así como los manuales técnico y de usuario

--- APP/frontend/src/App.jsx
+++ APP/frontend/src/App.jsx
@@ -1,26 +1,70 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
+import React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import {useEffect, useState} from 'react';
 import './App.css';
-import {Greet, Cpu, Disk } from "../wailsjs/go/main/App";
+import {Cpu, Disk } from "../wailsjs/go/main/App";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+    const [data, setData] = useState(0);
+    const [data1, setData1] = useState([0,0]);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+    const updateResultText = (newValue) => setData(newValue);
+
+    const updateResultText1 = (newValue) => {
+        setData1([newValue[0]/(1024*1024*1024), newValue[1]/(1024*1024*1024)]);
+    };
+
+    useEffect(() => {
+        setInterval(() => {
+            Cpu().then(updateResultText);
+            Disk().then(updateResultText1);
+        }, 2000);
+    }, []);
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+            <>
+            <div className="card mb-3 Card container">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h2 className="card-title CardTexts">Proyecto 1</h2>
+                            <h2 className="card-text CardTexts">Diego Andrés Obín Rosales</h2>
+                            <h2 className="card-text CardTexts">201903865</h2>
+                            <h2 className="card-text CardTexts"><small className="text-muted CardTexts">Sistemas operativos 2</small></h2>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div className='container ButtonsHome'>
+                <h1>CPU</h1>
+                <div>
+                    <h3>{data.toFixed(2)}%</h3>
+                </div>
+                <h1>Memoria</h1>
+                <div style={{display:"flex", justifyContent:"center"}}>
+                    <table style={{border: "2px solid white"}}>
+                        <thead>
+                            <tr>
+                            <th>Libre</th>
+                            <th>En Uso</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>{data1[0].toFixed(2)}Gb</td>
+                            <td>{data1[1].toFixed(2)}Gb</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                </div>
+            </div>
+        </>
         </div>
     )
 }
